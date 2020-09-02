b2b6dbc00abd71bc08c67ad93eda10812e0d6084
ptchacon <50625739+ptchacon@users.noreply.github.com>
2020-09-02 09:44:03 -0500

    Update iointeraction.py
    Now use Jupyter Notebook as basis of interacting through Braille displays.

--- Tools/iointeraction.py
+++ Tools/iointeraction.py
@@ -1,23 +1,15 @@
 """
-A place to build code for adding database data to the db
+This program is super-simple.
+It assume you have jupyter installed.
+Then it takes you directly to Jupyter Notebook Dashboard
+in Jnotes folder and thus
+makes for easy and full access to Python code 
+for Braille displays because your default browser and 
+your screen reader software are already tied together.
 """
 
 # import from Python libraries
-import pprint, subprocess, sys, os, inspect, importlib
+import subprocess, os
 
-# Third party libraries
-import wx, sqlalchemy, sqlalchemy.orm
-
-# Start redirection to output.txt file for 
-# the duration of this program.
-ofile = open("output.txt", "w")
-sys.stdout = ofile
-
-"""
-Here you put in code intended for typical print output but 
-because of this program, after you finish typing code,
-it will launch notepad with your output for inspection.
-"""
-        
-ofile.close()
-subprocess.run(["notepad", "output.txt"])
+os.chdir("../Jnotes")
+subprocess.Popen(["jupyter", "notebook"], creationflags=subprocess.CREATE_NEW_CONSOLE)
